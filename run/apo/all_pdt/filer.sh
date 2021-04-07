303c9b66e6a20fb4eba73f929bd67f47db6e2e99
pgbarletta <pbarletta@gmail.com>
2021-04-07 15:22:54 -0600

    APO production data

---
+++ run/apo/all_pdt/filer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pdb1=apo
+pdb2=apo
+
+for PH in 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0 6.5 7.0 7.5
+do
+    mkdir ${PH}
+
+    sed "s/remdtrajvalues 3.0/remdtrajvalues ${PH}/g" plantillas/get_ph_cpp > ${PH}/get_ph_cpp
+    sed -i "s/${pdb1}_3.0/${pdb2}_${PH}/g" ${PH}/get_ph_cpp
+    sed -i "s/${pdb1}/${pdb2}/g" ${PH}/get_ph_cpp
+
+    cd ${PH}
+    cpptraj < get_ph_cpp
+    cd ..
+
+done
+exit 0
