a33a4d2863cc10cf91940feeb35fcb93aa698515
pgbarletta <pbarletta@gmail.com>
2021-04-08 11:20:51 -0600

    APO vol data

---
+++ run/apo/cavidad/filer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+pdb="apo"
+k=0
+
+for i in `cat lista`
+do
+    k=`expr $k + 1`
+    ph=$(echo $i | cut -d ";" -f 1)
+    phh=$(echo $i | cut -d ";" -f 2)
+    
+    # vol por idx
+#    mkdir ${k}
+#    cd ${k}
+#    ANA2 ../plantillas/fix_cut_top_apo_10.pdb -c ../plantillas/obp.cfg -d /home/pbarletta/labo/20/reobp/run/${pdb}/pdt/${ph}ph/${pdb}_${k}.nc -o vol_trj_${pdb}_${k}
+#    cd ..
+
+    # vol por pH
+    mkdir ${phh}
+    cd ${phh}
+    echo ${phh}
+    ANA2 ../plantillas/fix_cut_top_apo_10.pdb -c ../plantillas/obp.cfg -d /home/pbarletta/labo/20/reobp/run/${pdb}/all_pdt/${phh}/${pdb}_${phh}_avg.nc -o vol_trj_${pdb}_${phh}
+    cd ..
+done
+
+exit 0
